0c121497ad8b311c228cb0f493d3cd7c31b8824c
Hotaru Uesugi <hotaruuesugi@HotarunoMacBook-Air.local>
2019-01-02 11:15:23 +0900

    Segmented Component

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Appbar from "./src/components/Appbar";
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/elements/CircleButton';
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoList />
-        <CircleButton>+</CircleButton>
+        <MemoListScreen />
       </View>
     );
   }
